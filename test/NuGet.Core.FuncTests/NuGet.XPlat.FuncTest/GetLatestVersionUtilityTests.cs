d8da3a0df0c18db0b1b110871e726250bff44bf9
martinrrm <t-marui@microsoft.com>
2020-07-02 16:36:44 -0700

    Added Tests
    Added test for GetLatestVersionUtilities

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/GetLatestVersionUtilityTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/GetLatestVersionUtilityTests.cs
@@ -3,18 +3,24 @@
 
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using NuGet.CommandLine.XPlat.Utility;
+using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Configuration.Test;
+using NuGet.Packaging.Core;
 using NuGet.Test.Utility;
+using NuGet.Versioning;
 using Xunit;
 
 namespace NuGet.XPlat.FuncTest
 {
+    [Collection("NuGet XPlat Test Collection")]
     public class GetLatestVersionUtilityTests
     {
         [Fact]
-        private void EvaluateSources_Success()
+        public void EvaluateSources_Success()
         {
             using (var mockBaseDirectory = TestDirectory.Create())
             {
@@ -34,7 +40,7 @@ namespace NuGet.XPlat.FuncTest
                                     </Contoso>
                                 </packageSourceCredentials>
                             </configuration>";
-                
+
                 var configPath = Path.Combine(mockBaseDirectory, nugetConfigFileName);
                 SettingsTestUtils.CreateConfigurationFile(nugetConfigFileName, mockBaseDirectory, config);
                 var settingsLoadContext = new SettingsLoadingContext();
@@ -45,6 +51,164 @@ namespace NuGet.XPlat.FuncTest
                 // Asert
                 Assert.Equal(2, result.Count);
                 Assert.NotEqual(null, result[0].Credentials);
+            }
+        }
+
+        public static readonly List<object[]> GetLatesVersionFromSourcesData
+            = new List<object[]>
+            {
+                    new object[] { new string[] { "0.0.5;0.9.0;1.0.0-preview.3;PackageX" }, new string[] { "0.0.5;0.9.0;PackageX" }, "1.0.0-preview.3", true, "PackageX" },
+                    new object[] { new string[] { "0.0.5;0.9.0;1.0.0-preview.3;PackageX" }, new string[] { }, "1.0.0-preview.3", true, "PackageX" },
+                    new object[] { new string[] { "0.0.5;0.9.0;1.0.0-preview.3;PackageX" }, new string[] { }, "0.9.0", false, "PackageX" },
+                    new object[] { new string[] { "0.0.5;0.9.0;PackageX" }, new string[] { }, "0.9.0", true, "PackageX" },
+                    new object[] { new string[] { "0.0.5;0.9.0;1.0.0-preview.3;PackageX", "0.0.5;0.9.0;2.0.0-preview.4;PackageY" },
+                        new string[] { "0.0.5;0.9.0;PackageX" }, "1.0.0-preview.3", true, "PackageX" },
+                    new object[] { new string[] { "0.0.5;0.9.0;1.0.0-preview.3;PackageX", "0.0.5;0.9.0;2.0.0-preview.4;PackageY" },
+                        new string[] { "0.0.5;0.9.0;PackageX" }, "2.0.0-preview.4", true, "PackageY" },
+            };
+
+        [Theory]
+        [MemberData(nameof(GetLatesVersionFromSourcesData))]
+        public async Task GetLatesVersionFromSources_Success(string[] sourceA, string[] sourceB, string expectedVersion, bool prerelease, string package)
+        {
+            using (var testDirectory = TestDirectory.Create())
+            {
+                // Arange
+                var sourceAPath = await GetSourceWithPackages(sourceA, testDirectory, "SourceA");
+                var sourceBPath = await GetSourceWithPackages(sourceB, testDirectory, "SourceB");
+
+                var sources = new PackageSource[] { new PackageSource(sourceAPath), new PackageSource(sourceBPath) };
+
+                // Act
+                var logger = NullLogger.Instance;
+                var result = await GetLatestVersionUtility.GetLatestVersionFromSources(sources, logger, package, prerelease);
+
+                //Asert
+                Assert.Equal(new NuGetVersion(expectedVersion), result);
+            }
+        }
+
+        public static readonly List<object[]> GetLatesVersionFromSourcesData_Error
+            = new List<object[]>
+            {
+                    new object[] { new string[] { "0.0.5;0.9.0;1.0.0-preview.3;PackageX", "0.0.5;0.9.0;2.0.0-preview.4;PackageY" },
+                        new string[] { "0.0.5;0.9.0;PackageX" }, true, "PackageZ" },
+            };
+
+        [Theory]
+        [MemberData(nameof(GetLatesVersionFromSourcesData_Error))]
+        public async Task GetLatesVersionFromSources_Error(string[] sourceA, string[] sourceB, bool prerelease, string package)
+        {
+            using (var testDirectory = TestDirectory.Create())
+            {
+                // Arange
+                var sourceAPath = await GetSourceWithPackages(sourceA, testDirectory, "SourceA");
+                var sourceBPath = await GetSourceWithPackages(sourceB, testDirectory, "SourceB");
+
+                var sources = new PackageSource[] { new PackageSource(sourceAPath), new PackageSource(sourceBPath) };
+
+                // Act
+                var logger = NullLogger.Instance;
+                var result = await GetLatestVersionUtility.GetLatestVersionFromSources(sources, logger, package, prerelease);
+
+                //Asert
+                Assert.Equal(null, result);
+            }
+        }
+
+        private static async Task<string> GetSourceWithPackages(string[] source, TestDirectory testDirectory, string path)
+        {
+            var sourcePath = Path.Combine(testDirectory.Path, path);
+            for (var i = 0; i < source.Count(); i++)
+            {
+                var packageInfo = source[i].Split(';');
+                var packages = new SimpleTestPackageContext[packageInfo.Count() - 1];
+                for (var j = 0; j < packageInfo.Count() - 1; j++)
+                {
+                    packages[j] = new SimpleTestPackageContext(packageInfo.Last(), packageInfo[j]);
+                }
+                await SimpleTestPackageUtility.CreatePackagesAsync(sourcePath, packages);
+
+            }
+            return sourcePath;
+        }
+
+        [Fact]
+        public async Task GetLatestVersionFromSources_WithMultipleSources_SelectsLatestVersion()
+        {
+            using (var testDirectory = TestDirectory.Create())
+            {
+                //Arrange
+                var sourceAPath = Path.Combine(testDirectory.Path, "SourceA");
+                var sourceBPath = Path.Combine(testDirectory.Path, "SourceB");
+
+                var packageX100 = new SimpleTestPackageContext("packageX", "1.0.0");
+                var packageX101 = new SimpleTestPackageContext("packageX", "1.0.1");
+
+                await SimpleTestPackageUtility.CreatePackagesAsync(sourceAPath, packageX100);
+                await SimpleTestPackageUtility.CreatePackagesAsync(sourceBPath, packageX101);
+
+                var sources = new PackageSource[] { new PackageSource(sourceAPath), new PackageSource(sourceBPath) };
+
+                //Act
+                var logger = NullLogger.Instance;
+                var result = await GetLatestVersionUtility.GetLatestVersionFromSources(sources, logger, packageX101.Id, false);
+
+                //Asert
+                Assert.Equal(packageX101.Identity.Version, result);
+            }
+        }
+
+        [Fact]
+        public async Task GetLatestVersionFromSources_WithMultipleSources_PackageNotAvailable()
+        {
+            using (var testDirectory = TestDirectory.Create())
+            {
+                //Arrange
+                var sourceAPath = Path.Combine(testDirectory.Path, "SourceA");
+                var sourceBPath = Path.Combine(testDirectory.Path, "SourceB");
+
+                var packageX100 = new SimpleTestPackageContext("packageX", "1.0.0");
+                var packageX101 = new SimpleTestPackageContext("packageX", "1.0.1");
+
+                await SimpleTestPackageUtility.CreatePackagesAsync(sourceAPath, packageX100);
+                await SimpleTestPackageUtility.CreatePackagesAsync(sourceBPath, packageX101);
+
+                var sources = new PackageSource[] { new PackageSource(sourceAPath), new PackageSource(sourceBPath) };
+
+                //Act
+                var logger = NullLogger.Instance;
+                var result = await GetLatestVersionUtility.GetLatestVersionFromSources(sources, logger, "packageY", false);
+
+                //Asert
+                Assert.Equal(null, result);
+            }
+        }
+
+        [Fact]
+        public async Task GetLatestVersionFromSources_WithMoreSourcesThanProcessorCount()
+        {
+            using (var testDirectory = TestDirectory.Create())
+            {
+                //Arrange
+                var processors = 16;
+                var sources = new PackageSource[16];
+                var packages = new SimpleTestPackageContext[16];
+                for (var i = 0; i < processors; i++)
+                {
+                    var sourcePath = Path.Combine(testDirectory.Path, "Source" + i.ToString());
+                    var packageX = new SimpleTestPackageContext("packageX", "1.0." + i.ToString());
+                    await SimpleTestPackageUtility.CreatePackagesAsync(sourcePath, packageX);
+                    sources[i] = new PackageSource(sourcePath);
+                    packages[i] = packageX;
+                }
+
+                //Act
+                var logger = NullLogger.Instance;
+                var result = await GetLatestVersionUtility.GetLatestVersionFromSources(sources, logger, packages.Last().Id, false);
+
+                //Asert
+                Assert.Equal(packages.Last().Identity.Version, result);
             }
         }
     }
